4ef85e9fc95631114c3ef58046bb5a06065b7aa3
Fred <frederikpriesshansen@gmail.com>
2026-09-08 13:16:04 +0200

    tilføjet star wars api tilføjet rick & morty api

--- app.js
+++ app.js
@@ -17,3 +17,57 @@ fetch("https://icanhazdadjoke.com", {
     .catch(error => {
         console.error("Fejl:", error)
     })
+
+const listElement = document.querySelector(".people")
+
+fetch("https://swapi.dev/api/people", {
+    headers: { "Accept": "application/json" }
+}
+)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("kunne ikke hente data")
+        }
+        return response.json();
+    })
+
+    .then(data => {
+        listElement.innerHTML =
+            data.results
+                .map(results => `
+                    <li>${results.name} (${results.height}: høj) hudfarve: ${results.skin_color}. hår farve: ${results.hair_color} </li>
+                `)
+                .join("")
+    })
+
+    .catch(error => {
+        console.error("Fejl:", error)
+    })
+
+    /* rick/morty */
+
+        const listRickElement = document.querySelector(".people-rick")
+
+fetch("https://rickandmortyapi.com/api/character", {
+    headers: { "Accept": "application/json" }
+}
+)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("kunne ikke hente data")
+        }
+        return response.json();
+    })
+
+    .then(data => {
+        listRickElement.innerHTML = 
+            data.results
+                .map(results => `
+                    <li>${results.name} (${results.gender}: <img src="${results.image}" alt=""> )  </li>
+                `)
+                .join("")
+    })
+
+    .catch(error => {
+        console.error("Fejl:", error)
+    })
